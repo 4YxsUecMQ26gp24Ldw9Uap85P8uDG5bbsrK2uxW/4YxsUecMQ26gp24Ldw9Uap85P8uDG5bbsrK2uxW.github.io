0dc5f6db6865d8fc1673e9934d8e7ab29aa446c5
4YxsUecMQ26gp24Ldw9Uap85P8uDG5bbsrK2uxW <85970995+4YxsUecMQ26gp24Ldw9Uap85P8uDG5bbsrK2uxW@users.noreply.github.com>
2022-05-11 21:13:54 -0400

    Add files via upload

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/?peqXHsCGPXcza2zy8SnJH5GmMtFsApCJmc4kwk5AzSdxaFPf6p9D8WHj7bdVnRpJc2X5ZCGBe8DuBL2CnsnC85EAmst47AscLLFNGy9LUqQ8F5Yhwwq2936FyFUDrudvvbtmWbwRrH48MDR4mSLymVxxrjRanQgcc2pvj8PQ2z9LT5WrNU6w6dnX3nPRUz7a5Yg7yUQmBakeUsQn4mNRSgEzEBDSPVWJeeb6j8nAnnzytEpDwSYA7yAqKDtTnzbc=',
+    bare: 'https://6w3u4yskvf5uq8b27qz8k7p5g2379kn9hcmyc4pmwtmea3ek27er2kgb327z2ep.aeroisass.com',
+    encodeUrl: Ultraviolet.codec.plain.encode,
+    decodeUrl: Ultraviolet.codec.plain.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
